eed8315df18fce39b5869ec540414c1962688e95
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-04-25 23:35:22 -0700

    Add missing generic type params, remove Java 8

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewPackage.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewPackage.java
@@ -1,7 +1,8 @@
 package com.reactnativecommunity.webview;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -10,18 +11,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class RNCWebViewPackage implements ReactPackage {
+  @NonNull
   @Override
-  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    return Collections.singletonList(new RNCWebViewModule(reactContext));
+  public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+    return Collections.<NativeModule>singletonList(new RNCWebViewModule(reactContext));
   }
 
-  // Deprecated from RN 0.47
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
+  @NonNull
   @Override
-  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    return Collections.singletonList(new RNCWebViewManager());
+  public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+    return Collections.<ViewManager>singletonList(new RNCWebViewManager());
   }
 }
